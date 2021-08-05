e5b92abf524161146273243ec2e3b211ee463590
Usu2 <usu2@example.com>
2021-08-05 16:57:15 +0200

    Revertir "First draft of the engine. Player's basic phisics, blocks, spikes, saves and teleports are done. Platforms are done too but don't work similar to the original engine."
    Esto revierte la confirmación d9ba3cc3178b01889688f4abe5c6ff2b0ac78b7e.

--- Src/Scripts/Menu.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-func _input(event):
-	if event.is_action_pressed("shoot"):
-		Global.load_game()
--- Src/Scripts/Spike.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-
-
-func _on_Spike_body_entered(body):
-	Global.die(body)
