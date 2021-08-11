77404b04f16d3f6e384c1fef0a0676317daa9abd
wareya <wareya@gmail.com>
2021-08-11 06:06:15 -0400

    annoying pedantic bugfixes

--- Src/Scripts/Player.gd
+++ Src/Scripts/Player.gd
@@ -1,12 +1,13 @@
 extends KinematicBody2D
 
-var h_speed = 150
+var h_speed = 3
 var direction = 1
 
-var MAX_FALL_SPEED = 400
-var gravity = 1100
-var jump_force = -415
-var double_jump_force = -363
+var MAX_FALL_SPEED = 9
+var gravity = 0.4
+var jump_force = -8.5
+var double_jump_force = -7
+var jump_release_limit = 0.45
 
 var jumping = true
 var double_jump = true
@@ -23,11 +24,13 @@ onready var jump_sound = $JumpSound
 onready var d_jump_sound = $DoubleJumpSound
 onready var death_sound = $DeathSound
 
-func _physics_process(delta):
-	velocity.y += gravity * delta
+func _process(_delta):
+	get_input()
+	
+	velocity.y += gravity
 	if velocity.y > MAX_FALL_SPEED:
 		velocity.y = MAX_FALL_SPEED
-	get_input()
+	
 	if is_on_floor():
 		if not velocity.x == 0:
 			sprite.animation = "Run"
@@ -38,31 +41,49 @@ func _physics_process(delta):
 			sprite.animation = "Jump"
 		else:
 			sprite.animation = "Fall"
-	move_and_slide(velocity,Vector2(0,-1))
+	
+	# FIXME: move the entire movement solver to repeated move_and_collide calls instead of using move_and_slide
+	move_and_slide(velocity/_delta,Vector2(0,-1))
 	
 	if is_on_floor():
 		velocity.y = 0
 		jumping = false
 		double_jump = true
 	else:
-		 jumping = true
+		jumping = true
 	
 	if is_on_ceiling():
 		velocity.y = 0
+
+var frames_moving = 0
 
 func get_input():
 	velocity.x = 0
 	
 	if Input.is_action_pressed("right"):
-		velocity.x += h_speed
+		if frames_moving > 0:
+			velocity.x += h_speed
 		if direction == -1:
+			$Sprite.position.x += 1
+			$Hull.position.x += 2
+			#position.x -= 1
 			direction = 1
 			scale.x = -1
+			#$Hull.position.x = 2.5
+		frames_moving += 1
 	elif Input.is_action_pressed("left"):
-		velocity.x -= h_speed
+		if frames_moving > 0:
+			velocity.x -= h_speed
 		if direction == 1:
+			$Sprite.position.x -= 1
+			$Hull.position.x -= 2
+			#position.x += 1
 			direction = -1
 			scale.x = -1
+			#$Hull.position.x = 1.5
+		frames_moving += 1
+	else:
+		frames_moving = 0
 	
 	if Input.is_action_just_pressed("jump"):
 		if jumping and double_jump and not platform:
@@ -75,12 +96,13 @@ func get_input():
 			double_jump = true
 			jump_sound.play()
 	
-	if Input.is_action_just_released("jump") and velocity.y < -130:
-		velocity.y = -130
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = velocity.y*0.45
 	
 	if Input.is_action_just_pressed("shoot"):
 		if bullets < max_bullets:
 			var new_bullet = bullet.instance()
+			new_bullet.parent = self
 			new_bullet.position = position
 			get_parent().add_child(new_bullet)
 			bullets += 1
